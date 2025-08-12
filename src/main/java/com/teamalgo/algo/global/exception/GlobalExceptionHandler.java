96537f78127b0bcbb56c70fa753e5a4919f993cd
haeun228 <123haeun@naver.com>
2025-08-12 14:07:13 +0900

    feat: 기본 API 응답 구조 및 예외 처리 구현

---
+++ src/main/java/com/teamalgo/algo/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.teamalgo.algo.global.exception;
+
+import com.teamalgo.algo.global.common.api.ApiResponse;
+import com.teamalgo.algo.global.common.code.ErrorCode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ApiResponse<Object>> handleCustomException(CustomException e) {
+        log.error("CustomException occurred: {}", e.getMessage());
+        return ApiResponse.fail(e.getErrorCode());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Object>> handleException(Exception e) {
+        ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+        return ApiResponse.fail(errorCode);
+    }
+}
+
