e0091c31aea7ff0053a63f4a44ea1a539dd02786
haeun228 <123haeun@naver.com>
2025-08-12 21:43:42 +0900

    feat: 구글 로그인 API 구현

---
+++ src/main/java/com/teamalgo/algo/auth/controller/AuthController.java
@@ -0,0 +1,40 @@
+package com.teamalgo.algo.auth.controller;
+
+import com.teamalgo.algo.auth.dto.LoginRequest;
+import com.teamalgo.algo.auth.dto.TokenResponse;
+import com.teamalgo.algo.auth.service.GoogleOAuthService;
+import com.teamalgo.algo.global.common.api.ApiResponse;
+import com.teamalgo.algo.global.common.code.SuccessCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final GoogleOAuthService googleService;
+
+    // 구글 로그인
+    @PostMapping("/google-login")
+    public ResponseEntity<ApiResponse<TokenResponse>> googleLogin(@RequestBody LoginRequest loginRequest) {
+        TokenResponse response = googleService.authenticateUser(loginRequest.getToken());
+        return ApiResponse.success(SuccessCode._OK, response);
+    }
+
+    // 카카오 로그인
+    @PostMapping("/kakao-login")
+    public ResponseEntity<ApiResponse<TokenResponse>> kakaoLogin(@RequestBody LoginRequest loginRequest) {
+
+        return ApiResponse.success(SuccessCode._OK, null);
+    }
+
+    // 깃허브 로그인
+    @PostMapping("/github-login")
+    public ResponseEntity<ApiResponse<TokenResponse>> githubLogin(@RequestBody LoginRequest loginRequest) {
+
+        return ApiResponse.success(SuccessCode._OK, null);
+    }
+
+}
