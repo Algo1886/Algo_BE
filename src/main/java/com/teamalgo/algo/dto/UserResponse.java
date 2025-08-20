57c4024377cbd9fb27017bef72d3d55bd0465983
haeun228 <123haeun@naver.com>
2025-08-20 14:21:32 +0900

    feat: UserResponse DTO 추가

---
+++ src/main/java/com/teamalgo/algo/dto/UserResponse.java
@@ -0,0 +1,24 @@
+package com.teamalgo.algo.dto;
+
+import com.teamalgo.algo.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class UserResponse {
+    private Long id;
+    private String username;
+    private String avatarUrl;
+
+    public static UserResponse from(User user) {
+        return UserResponse.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .avatarUrl(user.getAvatarUrl())
+                .build();
+    }
+
+}
