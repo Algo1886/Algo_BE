1b3b5bc25b291623f1c5d0efbe4839a3120da011
h2oung <wisekhy@ewhain.net>
2025-08-23 14:31:38 +0900

    핵심 아이디어랑 북마크 목록 추가

--- src/main/java/com/teamalgo/algo/dto/CoreIdeaDTO.java
+++ src/main/java/com/teamalgo/algo/dto/CoreIdeaDTO.java
@@ -3,23 +3,32 @@ package com.teamalgo.algo.dto;
 import com.teamalgo.algo.domain.record.RecordCoreIdea;
 import lombok.*;
 
+import java.util.List;
+
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class CoreIdeaDTO {
+
     private Long id;
     private String content;
     private Long recordId;
-    private String authorName;
+    private String problemTitle;
+    private List<String> categories;
 
     public static CoreIdeaDTO fromEntity(RecordCoreIdea entity) {
         return CoreIdeaDTO.builder()
                 .id(entity.getId())
                 .content(entity.getContent())
                 .recordId(entity.getRecord().getId())
-                .authorName(entity.getRecord().getUser().getUsername())
+                .problemTitle(entity.getRecord().getProblem().getTitle())
+                .categories(
+                        entity.getRecord().getRecordCategories().stream()
+                                .map(rc -> rc.getCategory().getName())
+                                .toList()
+                )
                 .build();
     }
 }
