08e9baef5a5e68f19e95de2fc793540abbe739a6
haeun228 <123haeun@naver.com>
2025-08-25 21:35:10 +0900

    chore: Docker 네트워크 생성 & Redis 설정 수정

--- src/main/java/com/teamalgo/algo/global/config/RedisConfig.java
+++ src/main/java/com/teamalgo/algo/global/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.teamalgo.algo.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -11,8 +12,10 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+    public RedisConnectionFactory redisConnectionFactory(
+            @Value("${REDIS_HOST}") String host,
+            @Value("${REDIS_PORT}") int port) {
+        return new LettuceConnectionFactory(host, port);
     }
 
     @Bean
