940c5de0c2f3a79794cc2061507c0b6e426237b0
haeun228 <123haeun@naver.com>
2025-09-16 16:43:22 +0900

    fix: 기록 목록 조회 API만 익명 접근 가능하도록 수정

--- src/main/java/com/teamalgo/algo/global/config/SecurityConfig.java
+++ src/main/java/com/teamalgo/algo/global/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.teamalgo.algo.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -44,10 +45,10 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/api/auth/**",
-                                "/api/records/**",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**"
                         ).permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/records").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, userService), UsernamePasswordAuthenticationFilter.class);
