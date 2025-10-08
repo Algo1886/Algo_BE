ed5148c0b0da16653a27ba0833c7bc2e2c742670
haeun228 <123haeun@naver.com>
2025-10-08 16:32:44 +0900

    chore: 운영 환경 Redis 비밀번호 설정 추가

--- src/main/java/com/teamalgo/algo/global/config/RedisConfig.java
+++ src/main/java/com/teamalgo/algo/global/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -14,8 +15,18 @@ public class RedisConfig {
     @Bean
     public RedisConnectionFactory redisConnectionFactory(
             @Value("${REDIS_HOST}") String host,
-            @Value("${REDIS_PORT}") int port) {
-        return new LettuceConnectionFactory(host, port);
+            @Value("${REDIS_PORT}") int port,
+            @Value("${REDIS_PASSWORD}") String password) {
+
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(host);
+        config.setPort(port);
+
+        if (password != null && !password.isBlank()) {
+            config.setPassword(password);
+        }
+
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean
